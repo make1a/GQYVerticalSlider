ed95a68146db0bc7616f63fe7761a59a9c6ecfce
make1a <869792674@qq.com>
2019-01-11 10:34:14 +0800

    Initial Commit

---
+++ MKSlider/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MKSlider
+//
+//  Created by Make on 2019/1/11.
+//  Copyright © 2019 Make. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MKSlider/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MKSlider
+//
+//  Created by Make on 2019/1/11.
+//  Copyright © 2019 Make. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
